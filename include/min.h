009f5226561fd9f58bb9c7425accc841b49cf2d0
walton <walton>
2004-09-03 15:59:11 +0000

    update

--- include/min.h
+++ include/min.h
@@ -2,7 +2,7 @@
 //
 // File:	min_data.h
 // Author:	Bob Walton (walton@deas.harvard.edu)
-// Date:	Thu Sep  2 20:29:19 EDT 2004
+// Date:	Fri Sep  3 10:03:20 EDT 2004
 //
 // The authors have placed this program in the public
 // domain; they make no warranty and accept no liability
@@ -11,16 +11,16 @@
 // RCS Info (may not be true date or author):
 //
 //   $Author: walton $
-//   $Date: 2004/09/03 09:37:16 $
+//   $Date: 2004/09/03 15:59:11 $
 //   $RCSfile: min.h,v $
-//   $Revision: 1.1 $
+//   $Revision: 1.2 $
 
 # define MIN_COMPACT_DATA 1
 # define MIN_32_BIT_INT int
 # define MIN_64_BIT_INT long long
 # define MIN_BIG_ENDIAN 0
 
-class min {
+struct min {
 
     typedef unsigned char uns8;
     typedef signed char int8;
@@ -34,6 +34,11 @@ class min {
     typedef unsigned MIN_64_BIT_INT uns64;
     typedef signed MIN_64_BIT_INT int64;
 
+    typedef double float64;
+
+    struct stub;
+    struct body;
+
 
 // If we have compact data, the 64 bit value is over-
 // loaded as an IEEE floating point number or as a 16
@@ -41,21 +46,61 @@ class min {
 //
 # if MIN_COMPACT_DATA
 
-    union gen64
-    {
-        float64 f;
-        struct {
-#	    if MIN_BIG_ENDIAN
-		uns16 h; // header
-		uns16 t; // type
-		uns32 v; // value
-#	    else
-		uns32 v; // value
-		uns16 t; // type
-		uns16 h; // header
-#	    endif
-	} s;
-    };
+    // A general datum can hold either an IEEE 64 bit
+    // floating point number, a pointer to a stub, or
+    // a 6 byte string.  The high order 16 bits deter-
+    // mine what is held.  There bits have one special
+    // value for pointers to stubs, another for strings,
+    // and all other values are for IEEE floating point
+    // numbers.
+
+    typedef uns64 gen64;
+
+    inline bool gen_is_float ( gen64 v ) {
+        return    (v & 0x7FFF000000000000)
+	       != 0x7FFF000000000000;
+    }
+    inline bool gen_is_stub ( gen64 v ) {
+        return    (v & 0xFFFF000000000000)
+	       == 0xFFFF000000000000;
+    }
+    inline bool gen_is_string ( gen64 v ) {
+        return    (v & 0xFFFF000000000000)
+	       == 0x7FFF000000000000;
+    }
+    inline float64 gen_to_float ( gen64 v ) {
+        return * (float64 *) & v;
+    }
+    inline stub * gen_to_stub ( gen64 v ) {
+        return (stub *) (unsigned)
+	       ( v & 0xFFFFFFFFFFFF );
+    }
+    // p[8] must be on 8 byte boundary.
+    inline void gen_to_string ( gen64 v, char p[8] ) {
+#	if MIN_BIG_ENDIAN
+	    * (uns64 *) p = v << 16;
+#	else
+	    * (uns64 *) p = v & 0xFFFFFFFFFFFF;
+#	endif
+    }
+    inline gen64 float_to_gen ( float64 v ) {
+        return * (gen64 *) & v;
+    }
+    inline gen64 stub_to_gen ( stub * p ) {
+        return   ((uns64) (unsigned) p)
+	       | 0xFFFF000000000000;
+    }
+    // p[8] must be on 8 byte boundary.
+    inline gen64 string_to_gen ( char p[8] ) {
+#	if MIN_BIG_ENDIAN
+	    return   ( ( * (uns64 *) p ) >> 16 )
+	           | 0x7FFF000000000000;
+#	else
+	    return   (   ( * (uns64 *) p )
+	               & 0xFFFFFFFFFFFF )
+	           | 0x7FFF000000000000;
+#	endif
+    }
 # endif
 
     struct body
@@ -66,7 +111,7 @@ class min {
     {
 	union {
 	    float64 f;
-	    uns8 c[8];
+	    char c[8];
 	    body * p;
 	    uns64 u;
 	    int64 i;
@@ -77,14 +122,59 @@ class min {
 	    struct {
 #		if MIN_BIG_ENDIAN
 		    uns8 t; // type
-		    uns8 pad[3];
+		    uns8 st; // subtype
+		    uns16 s;
 		    uns32 u;
 #		else
 		    uns32 u;
-		    uns8 pad[3];
+		    uns16 s;
+		    uns8 st; // subtype
 		    uns8 t; // type
 #		endif
 	    } s;
 	} g; // gc and control
     };
+
+    // Get value from a stub.
+    //
+    inline float64 get_float64 ( const stub * p ) {
+        return p->v.f;
+    }
+    inline int64 get_int64 ( const stub * p ) {
+        return p->v.i;
+    }
+    inline uns64 get_uns64 ( const stub * p ) {
+        return p->v.u;
+    }
+    inline body * get_body ( const stub * p ) {
+        return p->v.p;
+    }
+
+    // Put value into a stub.
+    //
+    inline void put_float64 ( stub * p, float64 v ) {
+        p->v.f = v;
+    }
+    inline void put_uns64 ( stub * p, uns64 v ) {
+        p->v.u = v;
+    }
+    inline void put_int64 ( stub * p, int64 v ) {
+        p->v.i = v;
+    }
+    inline void put_body ( stub * p, body * v ) {
+        p->v.p = v;
+    }
+
+    // Get type, chain, etc. from a stub.
+    //
+    inline unsigned get_type ( const stub * p ) {
+        return p->g.s.t;
+    }
+    inline unsigned get_subtype ( const stub * p ) {
+        return p->g.s.st;
+    }
+    inline stub * get_chain ( const stub * p ) {
+        return (stub *) (unsigned)
+	       ( p->g.u & 0xFFFFFFFFFFFF );
+    }
 };
