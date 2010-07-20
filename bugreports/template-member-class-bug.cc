59366de0bcd55e94d5ae89fdadf92c0daf2d5463
walton <walton>
2010-07-20 10:23:15 +0000

    update

--- bugreports/template-member-class-bug.cc
+++ bugreports/template-member-class-bug.cc
@@ -15,30 +15,47 @@ public:
     friend class A<T>;
 };
 
+// These are OK:
 
-template < typename T >
-void funcAvoid ( A<T> & c );
+    template < typename T >
+    void funcAvoid ( A<T> * a );
 
-template < typename T >
-int funcAint ( A<T> & c );
+    template < typename T >
+    int funcAint ( A<T> * a );
 
-template < typename T >
-void funcBvoid ( B<T> & c );
+    template < typename T >
+    void funcBvoid ( B<T> * b );
 
-template < typename T >
-int funcBint ( B<T> & c );
+    template < typename T >
+    int funcBint ( B<T> * b );
 
-template < typename T >
-void funcCvoid ( B<T>::C * c );
+// These incorrectly fail:
 
-template < typename T >
-int funcCint ( B<T>::C * c );
+    template < typename T >
+    void funcCvoid ( B<T>::C * c );
 
-template < typename T >
-void funcDvoid ( B<T>::D * d );
+    template < typename T >
+    int funcCint ( B<T>::C * c );
 
-template < typename T >
-int funcDint ( B<T>::D * d );
+    template < typename T >
+    void funcDvoid ( B<T>::D * d );
+
+    template < typename T >
+    int funcDint ( B<T>::D * d );
+
+// The following checks the non-template version:
+
+class E;
+class F
+{
+public:
+    class G;
+};
+
+// These non-template versions are OK:
+
+    void funcGvoid ( F::G * g );
+    int funcGint ( F::G * g );
 
 int main ()
 {
