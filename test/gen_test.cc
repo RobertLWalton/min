fd19be046a19891f08ef0aae74b530fdb644353f
Robert L. Walton <walton@acm.org>
2011-03-16 20:00:19 -0400

    update

--- test/gen_test.cc
+++ test/gen_test.cc
@@ -2,20 +2,21 @@
 //
 // File:	gen_test.cc
 // Author:	Bob Walton (walton@acm.org)
-// Date:	Wed Mar 16 16:05:46 EDT 2011
+// Date:	Wed Mar 16 19:59:55 EDT 2011
 //
 // The authors have placed this program in the public
 // domain; they make no warranty and accept no liability
 // for this program.
 
 // This test program shows that the gen struct below is
-// treated in assmebly code as if it were an unsigned long
-// long, and therefore is as efficient as an unsigned long
-// long.
+// treated in assmebly code as if it were an unsigned
+// long long, and therefore is as efficient as an
+// unsigned long long.
 //
 // Compile with g++ -c [-O] -S gen_test.cc
 
 #include <iostream>
+#include <cassert>
 using namespace std;
 
 typedef unsigned long long unsgen ;
@@ -67,4 +68,23 @@ inline gen inc ( gen x )
 gen addtwo ( gen v )
 {
     return test ( v ) ? v : inc ( inc ( v ) );
+}
+
+struct sizetest {
+    unsgen x;
+    gen y;
+    unsgen z;
+};
+
+int main()
+{
+    assert ( sizeof ( unsgen ) == sizeof ( gen ) );
+    assert (    10 * sizeof ( unsgen )
+             == sizeof ( gen[10] ) );
+    assert (    3 * sizeof ( unsgen )
+             == sizeof ( sizetest ) );
+    gen x = unprotected::new_gen ( 5 );
+    x = addtwo ( x );
+    assert ( unprotected::value_of ( x ) == 7 );
+    cout << "OK" << endl;
 }
