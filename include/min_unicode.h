ae6df93d7116dd776c999acf889e780bdf63bae9
Robert L Walton <walton@acm.org>
2014-07-16 16:08:51 -0400

    update

--- include/min_unicode.h
+++ include/min_unicode.h
@@ -2,7 +2,7 @@
 //
 // File:	min_unicode.h
 // Author:	Bob Walton (walton@acm.org)
-// Date:	Tue Jul 15 12:45:32 EDT 2014
+// Date:	Wed Jul 16 11:52:51 EDT 2014
 //
 // The authors have placed this program in the public
 // domain; they make no warranty and accept no liability
@@ -27,7 +27,7 @@ namespace min {
     using min::unicode::UNKNOWN_UCHAR;
     using min::unicode::utf8_to_unicode;
     using min::unicode::unicode_to_utf8;
-    using min::unicode::SOFTWARE_NL_CATEGORY;
+    using min::unicode::SOFTWARE_NL;
 
 }
 # endif
