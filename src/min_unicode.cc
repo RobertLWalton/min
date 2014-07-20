16b8229b037d5b1818e53b00cc19a70fdd1f4efc
Robert L Walton <walton@acm.org>
2014-07-19 21:53:30 -0400

    update

--- src/min_unicode.cc
+++ src/min_unicode.cc
@@ -2,7 +2,7 @@
 //
 // File:	min_unicode.cc
 // Author:	Bob Walton (walton@acm.org)
-// Date:	Tue Jul 15 12:45:24 EDT 2014
+// Date:	Sat Jul 19 21:26:03 EDT 2014
 //
 // The authors have placed this program in the public
 // domain; they make no warranty and accept no liability
@@ -16,44 +16,45 @@
 # define UNI min::unicode
 
 const unsigned
-    UNI::unicode_index_size = UNICODE_INDEX_SIZE;
+    UNI::index_size = UNICODE_INDEX_SIZE;
 const unsigned short
-    UNI::unicode_index[UNI::unicode_index_size] =
+    UNI::index[UNI::index_size] =
         { UNICODE_INDEX };
 const unsigned
-    UNI::unicode_index_limit = UNICODE_INDEX_LIMIT;
-const unsigned char
-    UNI::unicode_category[UNI::unicode_index_limit] =
+    UNI::index_limit = UNICODE_INDEX_LIMIT;
+const char * const
+    UNI::category[UNI::index_limit] =
         { UNICODE_CATEGORY };
-const UNI::ustring * const
-    UNI::unicode_name[UNI::unicode_index_limit] =
-        { UNICODE_NAME };
-const UNI::ustring * const
-    UNI::unicode_picture[UNI::unicode_index_limit] =
-        { UNICODE_PICTURE };
+short const
+    UNI::combining_class[UNI::index_limit] =
+        { UNICODE_COMBINING_CLASS };
+const char * const
+    UNI::bidi_class[UNI::index_limit] =
+        { UNICODE_BIDI_CLASS };
 const double
-    UNI::unicode_numerator[UNI::unicode_index_limit] =
+    UNI::numerator[UNI::index_limit] =
         { UNICODE_NUMERATOR };
 const double
-    UNI::unicode_denominator[UNI::unicode_index_limit] =
+    UNI::denominator[UNI::index_limit] =
         { UNICODE_DENOMINATOR };
 const double
-    UNI::unicode_numeric_value
-            [UNI::unicode_index_limit] =
+    UNI::numeric_value[UNI::index_limit] =
         { UNICODE_NUMERIC_VALUE };
+extern char const
+    UNI::bidi_mirrored[UNI::index_limit] =
+        { UNICODE_BIDI_MIRRORED };
+extern unsigned long long const
+    UNI::properties[UNI::index_limit] =
+        { UNICODE_PROPERTIES };
+const UNI::ustring * const
+    UNI::name[UNI::index_limit] =
+        { UNICODE_NAME };
+const UNI::ustring * const
+    UNI::picture[UNI::index_limit] =
+        { UNICODE_PICTURE };
+extern const char * const
+    UNI::support_set[UNI::index_limit] =
+        { UNICODE_SUPPORT_SET };
 const unsigned
-    UNI::unicode_reference_count
-            [UNI::unicode_index_limit] =
+    UNI::reference_count[UNI::index_limit] =
         { UNICODE_REFERENCE_COUNT };
-const char * const
-    UNI::unicode_category_name
-        [UNI::unicode_category_limit] =
-        { UNICODE_CATEGORY_NAME };
-const char * const
-    UNI::unicode_category_description
-	    [UNI::unicode_category_limit] =
-	        { UNICODE_CATEGORY_DESCRIPTION };
-const char * const
-    UNI::unicode_supported_set
-	    [UNI::unicode_supported_set_limit] =
-	        { UNICODE_SUPPORTED_SET };
